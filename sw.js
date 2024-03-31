09fe2de496f35d24a57e719b5b84cfdf9bb73cf5
Gabriel Cortés Garcia <tetradog@gmail.com>
2024-04-01 00:26:41 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,73 +1,72 @@
-const CACHE_VERSION_ANTERIOR = 6; //subo aqui para no tener problemas :D
-const CACHE_VERSION = CACHE_VERSION_ANTERIOR + 1;
+const CACHE_VERSION = 8;
 const APP = "PokemonOffsetToPointer";
+const CACHE_PREFIX = "CACHE_" + APP + "_";
 
-const CACHE_INMUTABLE = "CACHE_INMUTABLE_" + APP;
-const CACHE_SHELL = "CACHE_SHELL_" + APP;
-const CACHE_DINAMICO = "CACHE_DINAMICO_" + APP;
 const INMUTABLES = [
-
+  // Lista de recursos inmutables
 ];
-const SHELL = [
 
+const SHELL = [
+  // Lista de recursos shell
     "index.html",
     "images/icons/icon-144x144.png",
     "images/icons/icon-512x512.png",
     "sw.js",
     "pokemonOffsetToPointer.js",
     "manifest.json"
-
 ];
 
-
 self.addEventListener('install', e => {
-
-    var inmutables = self.FetchCache(CACHE_INMUTABLE + CACHE_VERSION, INMUTABLES);
-    var shell = self.FetchCache(CACHE_SHELL + CACHE_VERSION, SHELL);
-    console.log("installing version " + CACHE_VERSION);
-    e.waitUntil(Promise.all([inmutables, shell]));
-
+  console.log("Installing version " + CACHE_VERSION);
+  e.waitUntil(Promise.all([
+    cacheResources(CACHE_PREFIX + "INMUTABLE", INMUTABLES),
+    cacheResources(CACHE_PREFIX + "SHELL", SHELL)
+  ]));
 });
 
-
 self.addEventListener('activate', e => {
-    console.log("uninstalling version " + CACHE_VERSION_ANTERIOR);
-    e.waitUntil(Promise.all([caches.delete(CACHE_INMUTABLE + CACHE_VERSION_ANTERIOR),
-        caches.delete(CACHE_SHELL + CACHE_VERSION_ANTERIOR),
-        caches.delete(CACHE_DINAMICO + CACHE_VERSION)
-    ]));
-
+  console.log("Uninstalling version " + (CACHE_VERSION - 1));
+  e.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.filter(cacheName => {
+          return cacheName.startsWith(CACHE_PREFIX) && cacheName !== CACHE_PREFIX + "INMUTABLE";
+        }).map(cacheName => {
+          return caches.delete(cacheName);
+        })
+      );
+    })
+  );
 });
 
 self.addEventListener('fetch', e => {
-
-    e.respondWith(caches.match(e.request).then(resp => {
-        var respuesta;
-        if (resp)
-            respuesta = resp;
-        else {
-            respuesta = fetch(e.request)
-                .then(data => {
-                    return caches.open(CACHE_DINAMICO + CACHE_VERSION_ANTERIOR)
-                        .then(cache => {
-                            cache.put(e.request, data.clone());
-                            return data;
-                        });
-                });
-        }
-        return respuesta;
-
-    }));
-
+  e.respondWith(
+    caches.match(e.request).then(resp => {
+      return resp || fetchAndUpdateCache(e.request);
+    })
+  );
 });
 
+function cacheResources(cacheName, urls) {
+  return caches.open(cacheName).then(cache => {
+    return cache.addAll(urls);
+  });
+}
 
+function fetchAndUpdateCache(request) {
+  return fetch(request).then(response => {
+    if (!response || response.status !== 200 || response.type !== 'basic') {
+      return response;
+    }
 
-function FetchCache(cache_name, urls) {
-    return caches.open(cache_name)
-        .then(cache => {
+    const responseToCache = response.clone();
 
-            cache.addAll(urls);
+    caches.open(CACHE_PREFIX + "DINAMICO").then(cache => {
+      cache.put(request, responseToCache);
+    });
 
-        });
+    return response;
+  }).catch(error => {
+    console.error('Fetch failed:', error);
+  });
 }
